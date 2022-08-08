4b9dbde455c9041fad36146e151c0655c0ef001b
DDavid Gadyan <davit_gadyan20@alumni.aua.am>
2022-08-08 13:17:06 +0400

    API GET/POST/PUT/DELETE AUTH

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -13,6 +13,14 @@ jwt = JWT(app, authenticate, identity)
 items = []
 
 class Item(Resource):
+    # to parse and manage inputs in body
+    parser = reqparse.RequestParser()
+    parser.add_argument("price",
+                        type=float,
+                        required=True,
+                        help="This field cannot be left be blank!!"
+                        )
+
     @jwt_required() 
     def get(self, name):
         item = next(filter(lambda x: x["name"] == name, items), None)
@@ -22,7 +30,8 @@ class Item(Resource):
         if next(filter(lambda x: x["name"] == name, items), None):
             return {"message": "An item with the name {} already exists!!".format(name)}, 400
 
-        data = request.get_json()
+        data = Item.parser.parse_args()
+        # data = request.get_json()
         item = {"name": name, "price": data["price"]}
         items.append(item)
         return item, 201
@@ -33,7 +42,8 @@ class Item(Resource):
         return {"message": "Item deleted"}
 
     def put(self, name):
-        data = request.get_json()
+        data = Item.parser.parse_args()
+        # data = request.get_json()
         item = next(filter(lambda x: x["name"] == name, items), None)
         if item is None:
             item = {"name": name, "price": data["price"]}
